c04b7fde639080de30a07abb797b8abe64dd694b
jptorres1 <jptorres1@uc.cl>
2021-04-05 23:29:24 -0400

    views characters and episodes

--- breakingbad/urls.py
+++ breakingbad/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='breakingbad-home'),
+    path('episode/<int:pk>/', views.episode, name='breakingbad-episode'),
+    path('character/<str:name>/', views.character, name='breakingbad-character'),
 ]
--- breakingbad/views.py
+++ breakingbad/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import (
+    DetailView
+)
 import requests
 from itertools import groupby
 
@@ -16,3 +19,15 @@ def home(request):
     bb_seasons = [(grouper, list(values)) for grouper, values in bb_seasons]
 
     return render(request, 'breakingbad/home.html', {'BB': bb_seasons, 'BCS': bcs_seasons})
+
+
+def episode(request, pk):
+    episode = requests.get(f'https://tarea-1-breaking-bad.herokuapp.com/api/episodes/{pk}').json()[0]
+
+    return render(request, 'breakingbad/episode.html', {'episode': episode})
+
+def character(request, name):
+    character = requests.get(f'https://tarea-1-breaking-bad.herokuapp.com/api/characters?name={name}').json()[0]
+    quotes = requests.get(f'https://tarea-1-breaking-bad.herokuapp.com/api/quote?author={name}').json()
+
+    return render(request, 'breakingbad/character.html', {'character': character, 'quotes': quotes})
